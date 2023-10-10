0a1c6cfd5c294c983d474365024f88a7007fbcc5
Riyan042 <146145672+Riyan042@users.noreply.github.com>
2023-10-10 13:04:05 +0700

    Delete login directory

--- login/assets/javascript/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("show-hide").onclick = function() {
-    if (document.getElementById("password").type === "password") {
-        document.getElementById("password").type = "teks";
-    } else {
-        document.getElementById("password").type = "password";
-    }
-}
